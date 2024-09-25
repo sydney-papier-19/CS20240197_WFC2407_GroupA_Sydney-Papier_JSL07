c1a5f9dbdcf1c0afd1ec89351cadaa7d9650f4e4
sydney_p <sydneypapier23@gmail.com>
2024-09-25 10:05:46 +0200

    added some more commenting

--- index.js
+++ index.js
@@ -14,32 +14,38 @@ document.addEventListener('DOMContentLoaded', function () {
       const studentNameInput = document.getElementById('studentName');
       const personalMessageInput = document.getElementById('personalMessage') ;
       const courseNameInput = document.getElementById('courseName') ; 
-  
+      
+      //'toUpperCase()' converts the input values of studentName and courseName to uppercase  
       const studentName = studentNameInput.value.toUpperCase();
       const personalMessage = personalMessageInput.value;
       const courseName = courseNameInput ? courseNameInput.value.toUpperCase() : "a course"; // Fallback to "a course" if no input
-  
+      
+      //if the input values for student and course name are empty send an
+      // alert message 
       if (studentName.trim() === '' || personalMessage.trim() === '') {
         alert('Please fill in all fields');
         return;
       }
   
       // 🚨 Generate certificate content dynamically
+      //
       certificateContent.innerHTML = `
       <h2> Certificate of Achievement </h2>
       <p> This is to certify that</p>
-      <h3>${studentName}</h3>
-      <p> has almost completed the 
-      <h3>${courseName} Course</h3>
+      <h3>${studentName}</h3> 
+      <p> has almost completed the course
+      <h3>${courseName} </h3>
       <p>with legendary perseverance, tears and world-class bad-assery!🏆🔥</p>
       <img src="./logo.png" alt="Logo" style="width: 300px; height: auto; margin-bottom: 10px; padding: 20px;" >
       <p>${personalMessage}</p>
     `;
     
       //  Display the modal
+      //the moadal becomes visible by changing the display style from
+      //'none' to 'block'
       modal.style.display = 'block';
   
-      // Clear the form inputs
+      // Clears the form inputs once the form is submitted
       studentNameInput.value = '';
       personalMessageInput.value = '';
       if(courseNameInput) courseNameInput.value = '';
@@ -47,6 +53,7 @@ document.addEventListener('DOMContentLoaded', function () {
   
     //  🚨 Close the modal when the close button is clicked
     closeModal.addEventListener('click', function () {
+      //this hides the modal when clicked on
       modal.style.display = 'none';
     });
   });
